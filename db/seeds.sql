d9d07a6037883d6b57f4d2e833202e0ea459c50e
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-18 11:56:54 -0500

    outlining three sql tables with instructions info

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (id, name)
+VALUES (INT PRIMARY KEY, VARCHAR(30))
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (INT PRIMARY KEY, VARCHAR(30), DECIMAL, INT)
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (INT PRIMARY KEY, VARCHAR(30), VARCHAR(30), INT, INT or null)
