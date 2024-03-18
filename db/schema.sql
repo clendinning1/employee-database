b1391bfa9f8b128549e0ee58710fbd8ad424ae96
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-18 12:14:34 -0500

    created the role table and commented on the department and role tables

--- db/schema.sql
+++ db/schema.sql
@@ -5,9 +5,20 @@ USE employees_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    -- auto increment generates a unique number for each new insert --
     name VARCHAR(30) NOT NULL
+    -- varchar = a string up to the length of the variable --
 );
 
-CREATE TABLE role ();
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+
+    FOREIGN KEY (department_id)
+    -- key named department_id --
+    REFERENCES department(id)
+    -- the department_id key pulls from "id" in the department table --
+);
 
 CREATE TABLE employees ();
