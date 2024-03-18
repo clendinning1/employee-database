5e0f2911ca4990e749a5f8b88b869ec15d921e26
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-18 14:18:49 -0500

    fixed tables

--- db/schema.sql
+++ db/schema.sql
@@ -14,6 +14,7 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
+    department_id INT,
 
     FOREIGN KEY (department_id)
     -- key named department_id --
@@ -25,9 +26,9 @@ CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
 
     FOREIGN KEY (role_id)
     REFERENCES role(id)
-
-    manager_id INT
 );
