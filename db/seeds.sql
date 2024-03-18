e0be22bdd4fbd3b3f653b58dc09890539509eaf6
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-18 12:17:54 -0500

    cleared out seeds for later

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,4 +5,4 @@ INSERT INTO role (id, title, salary, department_id)
 VALUES ()
 
 INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (INT PRIMARY KEY, VARCHAR(30), VARCHAR(30), INT, INT or null)
+VALUES ()
