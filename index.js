1eecc95b4a9e3c047815860b4f13a28aaa3908bc
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-18 15:35:57 -0500

    cant figure out why seeds is broken so moving over to the inquirer for now. copy pasted funct from previous inquirer project, going to edit it down into what i need

---
+++ index.js
@@ -0,0 +1,146 @@
+
+// inquirer funct
+function init() {
+    const inquirer = require('inquirer');
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: "Enter text (up to 3 characters):",
+                name: 'inqTextInput',
+                // validating character input length
+                validate: function (input) {
+                    if (input.length >= 4) {
+                        return "Must enter 1-3 characters";
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: "Enter text color (all strings should be one word; include hashtag for hex code):",
+                name: 'inqTextColor',
+                validate: function (input) {
+                    const isValidColor = color => {
+                        // using regex to make sure the hex code is valid
+                        const hexPattern = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+                        return hexPattern.test(color);
+                    }
+                    const testInput = isValidColor(input);
+                    if (testInput === false) {
+                        // if the hex code isn't valid, check to see if the input is a css color name instead.
+                        // .toLowerCase ensures case insensitivity for the check :)
+                        if (colorNames.includes(input.toLowerCase())) {
+                            return true;
+                        } else {
+                            // if not, error
+                            return "Must enter a valid color";
+                        }
+                    } else {
+                        // if the hex code is valid, return "true" for the validator
+                        return true;
+                    }
+                }
+            },
+            {
+                type: 'checkbox',
+                message: "Choose your shape (will default to circle):",
+                choices: ["triangle", "circle", "square"],
+                name: 'inqShapeType',
+                validate: function (inqShapeType) {
+                    if (inqShapeType == "triangle" || "circle" || "square") {
+                        return true;
+                    } else {
+                        return "Please only select one shape";
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: "Enter shape color (all strings should be one word; include hashtag for hex code):",
+                name: 'inqShapeColor',
+                validate: function (input) {
+                    const isValidColor = color => {
+                        // All notes here are the same as the previous color input
+                        const hexPattern = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+                        return hexPattern.test(color);
+                    }
+                    const testInput = isValidColor(input);
+                    if (testInput === false) {
+                        if (colorNames.includes(input.toLowerCase())) {
+                            return true;
+                        } else {
+                            return "Must enter a valid color";
+                        }
+                    } else {
+                        return true;
+                    }
+                }
+            },
+        ])
+        .then((response) => {
+            // svg strings that are the same for each shape
+            const svg1 = "<svg width=\"300\" height=\"200\"> \n <style> \n .cirsq { \n font: bold 75px sans-serif; \n } .tri { \n font: bold 70px sans-serif; \n } \n </style> \n "
+            const svg2 = " \n </svg>"
+
+            if (response.inqShapeType == "triangle") {
+                // if the chosen shape was a triangle...
+                console.log("Building your triangle...");
+
+                // plugging in inputs from inquirer into svg file string
+                const newTriangle = new Triangle(response.inqTextInput, response.inqTextColor, response.inqShapeColor);
+                const fullTriangleString = svg1 + newTriangle.triangleString + svg2;
+
+                // creating the svg file
+                function createTriangle() {
+                    const fs = require("fs");
+
+                    fs.appendFile('logo.svg', fullTriangleString, (err) =>
+                        err ? console.error(err) : console.log("Generated logo.svg")
+                    );
+                }
+
+                createTriangle();
+
+            } else if (response.inqShapeType == "square") {
+                // if square... (same as above)
+                console.log("Building your square...");
+
+                const newSquare = new Square(response.inqTextInput, response.inqTextColor, response.inqShapeColor);
+                const fullSquareString = svg1 + newSquare.squareString + svg2;
+
+                function createSquare() {
+                    const fs = require("fs");
+
+                    fs.appendFile('logo.svg', fullSquareString, (err) =>
+                        err ? console.error(err) : console.log("Generated logo.svg")
+                    );
+                }
+
+                createSquare();
+
+            } else {
+                // if circle... (same as above)
+                console.log("Building your circle...");
+
+                const newCircle = new Circle(response.inqTextInput, response.inqTextColor, response.inqShapeColor);
+                const fullCirString = svg1 + newCircle.circleString + svg2;
+
+                function createCircle() {
+                    const fs = require("fs");
+
+                    fs.appendFile('logo.svg', fullCirString, (err) =>
+                        err ? console.error(err) : console.log("Generated logo.svg")
+                    );
+                }
+
+                createCircle();
+            }
+
+            return;
+        });
+}
+
+// calling funct
+init();
