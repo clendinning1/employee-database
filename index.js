7d7ad6e5e00be7293ba7157e9eb08c4a0187aa57
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-19 11:47:40 -0500

    commented out inquirer code borrowed from other project before merging

--- index.js
+++ index.js
@@ -9,73 +9,73 @@ function init() {
                 message: "What would you like to do?",
                 name: 'mainMenu',
                 choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role"],
-                validate: function (inqShapeType) {
-                    if (inqShapeType == "triangle" || "circle" || "square") {
-                        return true;
-                    } else {
-                        return "Please only select one shape";
-                    }
-                }
-            },
-        ])
-        .then((response) => {
-            // svg strings that are the same for each shape
-            const svg1 = "<svg width=\"300\" height=\"200\"> \n <style> \n .cirsq { \n font: bold 75px sans-serif; \n } .tri { \n font: bold 70px sans-serif; \n } \n </style> \n "
-            const svg2 = " \n </svg>"
+        //         validate: function (inqShapeType) {
+        //             if (inqShapeType == "triangle" || "circle" || "square") {
+        //                 return true;
+        //             } else {
+        //                 return "Please only select one shape";
+        //             }
+        //         }
+        //     },
+        // ])
+        // .then((response) => {
+        //     // svg strings that are the same for each shape
+        //     const svg1 = "<svg width=\"300\" height=\"200\"> \n <style> \n .cirsq { \n font: bold 75px sans-serif; \n } .tri { \n font: bold 70px sans-serif; \n } \n </style> \n "
+        //     const svg2 = " \n </svg>"
 
-            if (response.inqShapeType == "triangle") {
-                // if the chosen shape was a triangle...
-                console.log("Building your triangle...");
+        //     if (response.inqShapeType == "triangle") {
+        //         // if the chosen shape was a triangle...
+        //         console.log("Building your triangle...");
 
-                // plugging in inputs from inquirer into svg file string
-                const newTriangle = new Triangle(response.inqTextInput, response.inqTextColor, response.inqShapeColor);
-                const fullTriangleString = svg1 + newTriangle.triangleString + svg2;
+        //         // plugging in inputs from inquirer into svg file string
+        //         const newTriangle = new Triangle(response.inqTextInput, response.inqTextColor, response.inqShapeColor);
+        //         const fullTriangleString = svg1 + newTriangle.triangleString + svg2;
 
-                // creating the svg file
-                function createTriangle() {
-                    const fs = require("fs");
+        //         // creating the svg file
+        //         function createTriangle() {
+        //             const fs = require("fs");
 
-                    fs.appendFile('logo.svg', fullTriangleString, (err) =>
-                        err ? console.error(err) : console.log("Generated logo.svg")
-                    );
-                }
+        //             fs.appendFile('logo.svg', fullTriangleString, (err) =>
+        //                 err ? console.error(err) : console.log("Generated logo.svg")
+        //             );
+        //         }
 
-                createTriangle();
+        //         createTriangle();
 
-            } else if (response.inqShapeType == "square") {
-                // if square... (same as above)
-                console.log("Building your square...");
+        //     } else if (response.inqShapeType == "square") {
+        //         // if square... (same as above)
+        //         console.log("Building your square...");
 
-                const newSquare = new Square(response.inqTextInput, response.inqTextColor, response.inqShapeColor);
-                const fullSquareString = svg1 + newSquare.squareString + svg2;
+        //         const newSquare = new Square(response.inqTextInput, response.inqTextColor, response.inqShapeColor);
+        //         const fullSquareString = svg1 + newSquare.squareString + svg2;
 
-                function createSquare() {
-                    const fs = require("fs");
+        //         function createSquare() {
+        //             const fs = require("fs");
 
-                    fs.appendFile('logo.svg', fullSquareString, (err) =>
-                        err ? console.error(err) : console.log("Generated logo.svg")
-                    );
-                }
+        //             fs.appendFile('logo.svg', fullSquareString, (err) =>
+        //                 err ? console.error(err) : console.log("Generated logo.svg")
+        //             );
+        //         }
 
-                createSquare();
+        //         createSquare();
 
-            } else {
-                // if circle... (same as above)
-                console.log("Building your circle...");
+        //     } else {
+        //         // if circle... (same as above)
+        //         console.log("Building your circle...");
 
-                const newCircle = new Circle(response.inqTextInput, response.inqTextColor, response.inqShapeColor);
-                const fullCirString = svg1 + newCircle.circleString + svg2;
+        //         const newCircle = new Circle(response.inqTextInput, response.inqTextColor, response.inqShapeColor);
+        //         const fullCirString = svg1 + newCircle.circleString + svg2;
 
-                function createCircle() {
-                    const fs = require("fs");
+        //         function createCircle() {
+        //             const fs = require("fs");
 
-                    fs.appendFile('logo.svg', fullCirString, (err) =>
-                        err ? console.error(err) : console.log("Generated logo.svg")
-                    );
-                }
+        //             fs.appendFile('logo.svg', fullCirString, (err) =>
+        //                 err ? console.error(err) : console.log("Generated logo.svg")
+        //             );
+        //         }
 
-                createCircle();
-            }
+        //         createCircle();
+        //     }
 
             return;
         });
