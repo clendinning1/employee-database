22c44fcdfca9516d7ee02e1f19fbe7ae5bd6f1db
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-20 14:14:16 -0500

    restructuring so inquirer prompts are in their own file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // import submenus from subMenus.js
 const { viewDepartmentsQuery, viewRolesQuery, viewEmployeesQuery, addDepartmentQuery, addRoleQuery, addEmployeeQuery, updateEmployeeRoleQuery } = require('./lib/queries');
 
+const { mainMenuPrompt, addDepartmentPrompt, addRolePrompt, addEmployeePrompt, updateEmployeePrompt } = require('./lib/prompts');
+
 
 
 
@@ -10,14 +12,7 @@ function init() {
     const inquirer = require('inquirer');
     // this runs on startup and asks the following questions:
     inquirer
-        .prompt([
-            {
-                type: 'list',
-                message: "What would you like to do?",
-                name: 'mainMenu',
-                choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role"],
-            },
-        ])
+        .prompt(mainMenuPrompt)
         .then((response) => {
 
             if (response.mainMenu == "View All Departments") {
@@ -33,33 +28,26 @@ function init() {
                 viewEmployeesQuery();
 
             } else if (response.mainMenu == "Add a Department") {
-                // if you chose to Add a Department
-                inquirer
-                    .prompt([
-                        {
-                            type: 'input',
-                            message: "What is the name of the department?",
-                            name: 'addNewDepartment',
-                        },
-                    ])
+                // if you chose to Add a Department, it asks:
+                inquirer.prompt(addDepartmentPrompt)
                     .then((response) => {
-
+                        // then runs the following function:
                         addDepartmentQuery();
 
                         // CODE HERE: return to menu
                     });
 
             } else if (response.mainMenu == "Add a Role") {
-                // if you chose to Add a Role, run the following function
-                addRoleQuery();
+                // if you chose to Add a Role, it asks:
+                inquirer.prompt(addRolePrompt).then((response) => {});
 
             } else if (response.mainMenu == "Add an Employee") {
-                // if you chose to Add an Employee, run the following function
-                addEmployeeQuery();
+                // if you chose to Add an Employee, it asks:
+                inquirer.prompt(addEmployeePrompt).then((response) => { });
 
             } else {
-                // if you chose to Update an Employee Role, run the following function
-                updateEmployeeRoleQuery();
+                // if you chose to Update an Employee Role, it asks:
+                inquirer.prompt(updateEmployeePrompt).then((response) => { });
             }
         });
 }
